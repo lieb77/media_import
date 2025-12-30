8cf41b14b26d5f48fd73328c76cb582a89b2cb74
Paul Lieberman <lieb77@gmail.com>
2025-12-30 07:48:14 -0800

    Added the media lister

---
+++ src/Controller/MediaImportController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace \Drupal\media_import\Controller;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Drupal\media_import\MediaList;
+
+/**
+ *
+ */
+final class MediaImportController extends ControllerBase {
+
+    public function __construct(protected MediaList $lister) {}
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container): static {
+        return new static(
+            $container->get('media_import.list'),
+        );
+    }
+
+    public function getMedia(){
+        $media = $this->lister->getMediaInfo();
+
+        $header = [
+            $this->t("Id"),
+            $this->t("Media name"),
+            $this->t("Uri")
+        ]
+
+
+        // Construct the render array for the table.
+        $build['table'] = [
+          '#type'   => 'table',
+          '#header' => $header,
+          '#rows'   => $media,
+        ];
+
+        return $build
+
+    }
+
+//end-of-class
+}
